feeab73f9e255678f7001e74622f99cb0a7ee3d4
Gareth Jones <github@garethdjones.com>
2015-03-24 08:07:41 -0400

    Added correct App Store link.

--- ScoreTheFight/SettingsVC.swift
+++ ScoreTheFight/SettingsVC.swift
@@ -82,9 +82,7 @@ class SettingsVC: UIViewController {
     
     // Leave a review
     func leaveReview() {
-        if let checkURL = NSURL(string: "itms://itunes.apple.com/us/app/apple-store/id375380948?mt=8") {
-            // Actual link 
-            //"itms://itunes.apple.com/us/app/fight-scores/id975691413?ls=1&mt=8"
+        if let checkURL = NSURL(string: "itms://itunes.apple.com/us/app/fight-scores/id975691413?ls=1&mt=8") {
             if UIApplication.sharedApplication().openURL(checkURL) {
                 println("url successfully opened")
             }
